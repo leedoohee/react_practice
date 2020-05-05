9c07a4636c9d0a16dec5e4f9acca73b6442cf212
clampea4120 <clampea4120@gmail.com>
2020-05-05 16:58:03 +0900

    reducer 작성

---
+++ src/reducers/board/index.js
@@ -0,0 +1,29 @@
+
+
+import {combineReducers} from 'redux'
+
+
+const initState = {
+    contents : [
+        {title : 'aaa'}
+    ]
+}
+
+
+
+const contents = function(state = initState.contents, action) {
+
+    // switch(action.type){
+
+    // }
+    return state
+}
+
+
+
+
+
+
+export default combineReducers({
+    contents
+})
